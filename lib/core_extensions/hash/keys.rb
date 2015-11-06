f1e4658d7cd3aa50269dd7c6f570199c6666e92d
Daniel LeGrand <dan.legrand@gmail.com>
2015-11-06 08:00:49 -0500

    Use proper namespace for Hash class reference

--- lib/core_extensions/hash/keys.rb
+++ lib/core_extensions/hash/keys.rb
@@ -8,21 +8,22 @@ module CoreExtensions
     module Keys
       def rename_keys(mapping)
         result = {}
-        self.map do |k, v|
+
+        self.each do |k, v|
           mapped_key = mapping[k] ? mapping[k] : k
 
           # If this entry is a deep/nested hash, recursively change key names.
-          result[mapped_key] = v.class.to_s == "Hash" ? v.rename_keys(mapping) : v
+          result[mapped_key] = v.kind_of?(::Hash) ? v.rename_keys(mapping) : v
 
-          result[mapped_key] = v.collect{ |obj| obj.rename_keys(mapping) if obj.class.to_s == "Hash" } if v.class.to_s == "Array"
+          result[mapped_key] = v.collect{ |obj| obj.rename_keys(mapping) if obj.kind_of?(::Hash) } if v.kind_of?(::Array)
         end
 
         # If used within a Rails project, allow reference by either symbol or string.
         result.respond_to?(:with_indifferent_access) ? result.with_indifferent_access : result
-       end
+      end
     end
   end
 end
 
 # Actually monkey-patch the Ruby core Hash class
-Hash.include CoreExtensions::Hash::Keys
+Hash.include ::CoreExtensions::Hash::Keys
